43e4b37ac172db9a919184c059b3ba4559b0fe2d
wangqi <wangqi908@hotmail.com>
2021-03-16 09:04:48 +0800

    查询历史样式完成

--- src/views/home/components/search/components/history/History.vue
+++ src/views/home/components/search/components/history/History.vue
@@ -1,25 +1,28 @@
 <template>
-  <div>
-    <div v-for="item in list" :key="item.id">
-      {{ item.value }}--{{ item.id }}
-      <button @click="clear(item.id)">clear</button>
-    </div>
-  </div>
+  <ul v-for="item in list" :key="item.id">
+    <li class="item">
+      <span class="iconfont icon-search search"></span>
+      <span @click="search(item.value)" class="name">{{ item.value }}</span>
+      <div class="clear" @click="clear(item.id)">
+        <span class="iconfont icon-guanbi1"></span>
+      </div>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
+import { useStore } from 'vuex'
 import { getStorage, removeStorage } from '@/utils'
 
 export default defineComponent({
   setup () {
-    const searchValue = ref('')
+    const store = useStore()
     const list = ref([])
 
     function getValue () {
       const hotList = getStorage('hot')
-      console.log(hotList)
-      list.value = hotList || []
+      list.value = hotList ? hotList.reverse() : []
     }
 
     function clear (id: number) {
@@ -27,16 +30,46 @@ export default defineComponent({
       getValue()
     }
 
+    function search (value: string) {
+      store.commit('search/changeSearch', value)
+      store.commit('search/changeActionType', 'SONG_LIST')
+    }
+
     onMounted(() => {
       getValue()
     })
+
     return {
       clear,
       list,
-      searchValue
+      search
     }
   }
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.item {
+  display: flex;
+  align-items: center;
+  padding: 0 6px;
+  font-size: 13px;
+  border-bottom: 1px solid #ccc;
+  .search {
+    width: 20px;
+    margin-right: 10px;
+  }
+  .iconfont {
+    color: #ccc;
+  }
+  .clear {
+    padding: 8px 0;
+    text-align: center;
+    width: 40px;
+  }
+  .name {
+    flex: 9;
+    width: 100%;
+  }
+}
+</style>
